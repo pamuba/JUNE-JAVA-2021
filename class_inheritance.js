2251fc9c015c182d70ce0dedd05146acc986cfa3
pamuba <partha.bora24@gmail.com>
2021-09-15 00:10:53 +0530

    25

--- class_inheritance.js
+++ class_inheritance.js
@@ -4,12 +4,19 @@ class A{
     }
 }
 class B extends A{
-    constructor(y){
+    constructor(x,y){
+        super(x)
         this.b = y
     }
 }
 class C extends B{
-    constructor(z){
+    constructor(x,y,z){
+        super(x,y)
         this.c = z
     }
+    dis(){
+        console.log(`A:${this.a} B:${this.b} C:${this.c}`)
+    }
 }
+let c = new C(1,2,3)
+c.dis()
