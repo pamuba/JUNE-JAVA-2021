da70129c5beb3ddfec6ac3134536af71226a5dc0
pamuba <partha.bora24@gmail.com>
2021-09-17 00:08:06 +0530

    31

---
+++ arrowFns.js
@@ -0,0 +1,5 @@
+let add = (x,y)=>{
+    return `SUM:${x+y}`
+}
+
+console.log(add(11,22))
---
+++ generators.js
@@ -0,0 +1,19 @@
+// //generators
+// function* generator(){
+//    yield 1;
+//    yield 2;
+//    yield 3;
+//    yield 4;
+//    yield 5;
+// }
+
+// let gen = generator()
+
+// console.log(gen.next().value)
+// console.log(gen.next().value)
+// console.log(gen.next().value)
+// console.log(gen.next().value)
+// console.log(gen.next().value)
+
+
+// console.log(gen.next().value)
