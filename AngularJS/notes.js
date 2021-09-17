988e6beb590619fef2abd387d0268e69e66f61bb
pamuba <partha.bora24@gmail.com>
2021-09-18 00:19:18 +0530

    455

---
+++ AngularJS/notes.js
@@ -0,0 +1,11 @@
+// JS : interactivity to Static web sites
+
+// jQuery: DOM Manipulation with less js code
+
+//React
+
+//Vue
+
+// AngularJS : Extends HTML, reduces the amt of JS we write
+
+// it is a JS Framework that helps build web apps
