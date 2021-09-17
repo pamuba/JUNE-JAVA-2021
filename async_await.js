4b5a3b8fbfd4333ca5a25f4de42bbfb80e5fb4aa
pamuba <partha.bora24@gmail.com>
2021-09-17 23:55:35 +0530

    42

--- async_await.js
+++ async_await.js
@@ -11,3 +11,30 @@ function doubleAfter2Seconds(x){
 // doubleAfter2Seconds(10).then(
 //     (value)=>console.log(value)
 // )
+
+// let sum = doubleAfter2Seconds(10).then(res=>res) +  doubleAfter2Seconds(20).then(res=>res) + doubleAfter2Seconds(30).then(res=>res)
+// console.log(sum)
+
+// function addPromise(){
+//     return new Promise(resolve => {
+//         doubleAfter2Seconds(10).then((a)=>{
+//             doubleAfter2Seconds(20).then((b)=>{
+//                 doubleAfter2Seconds(30).then((c)=>{
+//                     resolve(a+b+c)
+//                 })
+//             })
+//         })
+//     })
+// }
+
+// addPromise().then(r=>console.log(r))
+
+async function addAsync(){
+    let a = await doubleAfter2Seconds(10)
+    let b = await doubleAfter2Seconds(20)
+    let c = await doubleAfter2Seconds(30)
+
+    return a+b+c
+}
+
+addAsync().then(r=>console.log(r))
