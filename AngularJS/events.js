b675c5d7b8ee03e3f0b00b6b139bca1aadb72847
pamuba <partha.bora24@gmail.com>
2021-09-21 00:03:06 +0530

    55

--- AngularJS/events.js
+++ AngularJS/events.js
@@ -10,10 +10,15 @@ var app = angular
                         { name:"GrapQL", likes:0, dislikes:0},
                     ];
 
+                    //1.
                     $scope.technologies = technologies;
+
+                    //2.
                     $scope.incrementLikes = function(tech){
                         tech.likes++
                     }
+
+                    //3.
                     $scope.incrementDisLikes = function(tech){
                         tech.dislikes++
                     }
