3f12d6b4f7538b99a3d03fb5d20da9992c90f1ab
pamuba <partha.bora24@gmail.com>
2021-09-20 23:59:07 +0530

    54

--- AngularJS/loops.js
+++ AngularJS/loops.js
@@ -10,4 +10,33 @@ myApp.controller("myController" , function($scope){
     ];
 
     $scope.employees = employees;
+
+    var countries = [
+        {
+            name: "UK",
+            cities: [
+                { name: "London" },
+                { name: "Birmingham" },
+                { name: "Manchester" }
+            ]
+        },
+        {
+            name: "USA",
+            cities: [
+                { name: "Los Angeles" },
+                { name: "Chicago" },
+                { name: "Houston" }
+            ]
+        },
+        {
+            name: "India",
+            cities: [
+                { name: "Hyderabad" },
+                { name: "Chennai" },
+                { name: "Mumbai" }
+            ]
+        }
+    ];
+
+    $scope.countries = countries;
 })
