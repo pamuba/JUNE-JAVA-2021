da350073712e958016bfabbdda33211ecb1270fb
pamuba <partha.bora24@gmail.com>
2021-09-25 00:11:09 +0530

    39

--- MEANJS/public/controllers/controller.js
+++ MEANJS/public/controllers/controller.js
@@ -1,27 +1,11 @@
-function AppCtrl($scope){
+//DI
+function AppCtrl($scope, $http){
     console.log("Hello from the controller")
 
-    person1 = {
-        name:"John",
-        email:"john@gmail.com",
-        number:"(111) 111-1111"
-    },
-    person2 = {
-        name:"JIll",
-        email:"jill@gmail.com",
-        number:"(222) 222-2222"
-    },
-    person3 = {
-        name:"Jim",
-        email:"jim@gmail.com",
-        number:"(333) 333-333"
-    },
-    person4 = {
-        name:"Jake",
-        email:"jake@gmail.com",
-        number:"(444) 444-4444"
-    }
+    $http.get('/contactlist').success(function(response){
+        $scope.contactlist = response
+    })
 
-    var contactlist = [person1, person2, person3, person4]
-    $scope.contactlist = contactlist
+    
+    
 }
--- MEANJS/server.js
+++ MEANJS/server.js
@@ -1,10 +1,35 @@
 var express = require('express')
 var app = express()
 
-// app.get("/", function(req, res){
-//     res.send("Hello World from the SERVER")
-// });
+
 app.use(express.static(__dirname+"/public"))
+
+
+app.get('/contactlist', function(req, res){
+    person1 = {
+        name:"John",
+        email:"john@gmail.com",
+        number:"(111) 111-1111"
+    },
+    person2 = {
+        name:"JIll",
+        email:"jill@gmail.com",
+        number:"(222) 222-2222"
+    },
+    person3 = {
+        name:"Jim",
+        email:"jim@gmail.com",
+        number:"(333) 333-333"
+    },
+    person4 = {
+        name:"Jake",
+        email:"jake@gmail.com",
+        number:"(444) 444-4444"
+    }
+
+    var contactlist = [person1, person2, person3, person4]
+    res.json(contactlist)
+})
 
 app.listen(3000, function(err){
     if(err)
