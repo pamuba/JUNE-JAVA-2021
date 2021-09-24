0630854abe4d48bcbf7eb1c3c11fcc1ce8e2f44b
pamuba <partha.bora24@gmail.com>
2021-09-25 00:23:16 +0530

    812

--- MEANJS/server.js
+++ MEANJS/server.js
@@ -1,4 +1,6 @@
 var express = require('express')
+var mongojs = require('mongojs')
+var db = mongojs('contactlist',['contactlist'])
 var app = express()
 
 
@@ -6,29 +8,14 @@ app.use(express.static(__dirname+"/public"))
 
 
 app.get('/contactlist', function(req, res){
-    person1 = {
-        name:"John",
-        email:"john@gmail.com",
-        number:"(111) 111-1111"
-    },
-    person2 = {
-        name:"JIll",
-        email:"jill@gmail.com",
-        number:"(222) 222-2222"
-    },
-    person3 = {
-        name:"Jim",
-        email:"jim@gmail.com",
-        number:"(333) 333-333"
-    },
-    person4 = {
-        name:"Jake",
-        email:"jake@gmail.com",
-        number:"(444) 444-4444"
-    }
-
-    var contactlist = [person1, person2, person3, person4]
-    res.json(contactlist)
+  
+    db.contactlist.find(function(err, docs){
+        if(err)
+            console.log("Error Starting the Server")
+        else 
+            res.json(docs)
+    })
+    
 })
 
 app.listen(3000, function(err){
