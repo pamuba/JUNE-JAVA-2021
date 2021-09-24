56fea56be57b449b1bba142962f6cc34885d1d9c
pamuba <partha.bora24@gmail.com>
2021-09-25 00:00:02 +0530

    74

--- MEANJS/public/controllers/controller.js
+++ MEANJS/public/controllers/controller.js
@@ -1,4 +1,4 @@
-function AppCtrl(){
+function AppCtrl($scope){
     console.log("Hello from the controller")
 
     person1 = {
@@ -6,19 +6,22 @@ function AppCtrl(){
         email:"john@gmail.com",
         number:"(111) 111-1111"
     },
-    person1 = {
+    person2 = {
         name:"JIll",
         email:"jill@gmail.com",
         number:"(222) 222-2222"
     },
-    person1 = {
+    person3 = {
         name:"Jim",
         email:"jim@gmail.com",
         number:"(333) 333-333"
     },
-    person1 = {
+    person4 = {
         name:"Jake",
         email:"jake@gmail.com",
         number:"(444) 444-4444"
     }
+
+    var contactlist = [person1, person2, person3, person4]
+    $scope.contactlist = contactlist
 }
