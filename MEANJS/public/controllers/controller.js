3fa090d467373c367594e1dc94fb15e11eda0d71
pamuba <partha.bora24@gmail.com>
2021-09-24 23:56:17 +0530

    72

---
+++ MEANJS/public/controllers/controller.js
@@ -0,0 +1,24 @@
+function AppCtrl(){
+    console.log("Hello from the controller")
+
+    person1 = {
+        name:"John",
+        email:"john@gmail.com",
+        number:"(111) 111-1111"
+    },
+    person1 = {
+        name:"JIll",
+        email:"jill@gmail.com",
+        number:"(222) 222-2222"
+    },
+    person1 = {
+        name:"Jim",
+        email:"jim@gmail.com",
+        number:"(333) 333-333"
+    },
+    person1 = {
+        name:"Jake",
+        email:"jake@gmail.com",
+        number:"(444) 444-4444"
+    }
+}
